e87cf479437726a93684ed5faf32f4a68573d081
Alvarez <alvxrz.rip@gmail.com>
2026-04-01 13:20:43 -0600

    fix: confirmación de conexión a api

--- backend/server.js
+++ backend/server.js
@@ -12,6 +12,13 @@ app.use("/api/alumnos", require("./src/routes/alumnos"));
 app.use("/api/grupos", require("./src/routes/grupos"));
 app.use("/api/calificaciones", require("./src/routes/calificaciones"));
 
+app.get('/', (req, res) => {
+  res.json({
+    mensaje: "API RCA activa",
+    version: "1.0"
+  });
+});
+
 app.listen(PORT, () => {
     console.log("Servidor corriendo en puerto " + PORT);
 });
