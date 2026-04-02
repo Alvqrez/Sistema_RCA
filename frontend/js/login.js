a2ca419ad4f5b8e5314ca45bf8b993b1c28d8fc0
Ingrid Aguilar <ingridaguilargonzalezagg08@gmail.com>
2026-04-02 16:08:03 -0600

    login mejorado

--- frontend/js/login.js
+++ frontend/js/login.js
@@ -1,13 +1,29 @@
-document.getElementById("loginForm").addEventListener("submit", function(e) {
+const form = document.getElementById("loginForm");
+const error = document.getElementById("error");
+
+form.addEventListener("submit", function(e) {
     e.preventDefault();
 
-    let user = document.getElementById("user").value;
-    let pass = document.getElementById("pass").value;
+    const user = document.getElementById("user").value;
+    const pass = document.getElementById("pass").value;
+    const rol = document.getElementById("rol").value;
 
     if (user === "admin" && pass === "1234") {
-        localStorage.setItem("login", "true");
-        window.location.href = "pages/alumnos.html";
+
+        // Guardar sesión
+        localStorage.setItem("usuario", user);
+        localStorage.setItem("rol", rol);
+
+        // Redirección según rol
+        if (rol === "admin") {
+            window.location.href = "pages/alumnos.html";
+        } else if (rol === "maestro") {
+            window.location.href = "pages/calificaciones.html";
+        } else {
+            window.location.href = "pages/alumnos.html";
+        }
+
     } else {
-        document.getElementById("error").textContent = "Datos incorrectos";
+        error.textContent = "Usuario o contraseña incorrectos";
     }
 });
