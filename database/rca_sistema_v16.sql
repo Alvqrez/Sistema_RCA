490cfc831b00e3082b9f6ab54894862e519e0fb8
Alvarez <alvxrz.rip@gmail.com>
2026-06-26 21:55:50 -0600

    fix: sql

--- database/rca_sistema_v16.sql
+++ database/rca_sistema_v16.sql
@@ -5,12 +5,7 @@ SET @OLD_FOREIGN_KEY_CHECKS=@@FOREIGN_KEY_CHECKS, FOREIGN_KEY_CHECKS=0;
 SET @OLD_SQL_MODE=@@SQL_MODE,
     SQL_MODE='ONLY_FULL_GROUP_BY,STRICT_TRANS_TABLES,NO_ZERO_IN_DATE,NO_ZERO_DATE,ERROR_FOR_DIVISION_BY_ZERO,NO_ENGINE_SUBSTITUTION';
 
--- ── Base de datos ─────────────────────────────────────────────────────────────
-DROP DATABASE IF EXISTS `rca_sistema`;
-CREATE SCHEMA `rca_sistema`
-  DEFAULT CHARACTER SET utf8mb4
-  COLLATE utf8mb4_spanish_ci;
-USE `rca_sistema`;
+
 
 
 -- ─────────────────────────────────────────────────────────────────────────────
